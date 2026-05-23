9654e472a93373b19047b7c3f1175da6bc34d8f0
A11akir <vova.abelis@yandex.ru>
2026-05-23 21:35:48 +0300

    [save and bug fix]

--- Assets/Feature/Hero/BaseHeroClass.cs
+++ Assets/Feature/Hero/BaseHeroClass.cs
@@ -2,14 +2,13 @@ namespace Feature.Hero
 {
     public enum BaseHeroClass
     {
-        None = 0,
         Alchemist = 1,
         Assassin = 2,
         EarthMage = 3,
         Explorer = 4,
         FireMage = 5,
         Monster = 6,
-        Warrior = 6,
+        Warrior = 7,
         WaterMage = 8,
         WindMage = 9,
         All = 10
